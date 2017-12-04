41e0bbfdb65b09485342821207a9450b5ab8b1f0
Unknown <nicolas.giannelos@epitech.eu>
2017-12-04 18:34:44 +0000

    [+] Integrated macro x to options
    - functional

--- include/options.h
+++ include/options.h
@@ -5,18 +5,33 @@
 ** Login SRJanel <n******.******s@epitech.eu>
 ** 
 ** Started on  Sat Oct 14 21:57:19 2017 
-** Last update Fri Nov 10 19:10:47 2017 
+** Last update Mon Dec  4 18:13:06 2017 
 */
 
 #ifndef OPTIONS_H_
 # define OPTIONS_H_
 
+# define STRINGIFY(x) #x
+# define MAKE_STRING(x) STRINGIFY(x)
+
+# define OPTIONS_WITH_ARG_TABLE						\
+  ENTRY(ftp, required_argument, 'f')					\
+  ENTRY(username, required_argument, 'u')				\
+  ENTRY(password, optional_argument, 'p')				\
+  ENTRY(target, required_argument, 't')
+
+# define OPTIONS_WITHOUT_ARG_TABLE		\
+  ENTRY(help, 'h')
+
+# define OPTSTRING	"t:f:u:p:h"
+
 extern struct s_options {
-  int	help;
-  char	*ftp;
-  char	*username;
-  char	*password;
-  char	*target;
+# define ENTRY(FIELD, UNUSED1, UNUSED2) char *FIELD;
+  OPTIONS_WITH_ARG_TABLE
+# undef ENTRY
+# define ENTRY(FIELD, UNUSED1) int FIELD;
+  OPTIONS_WITHOUT_ARG_TABLE
+# undef ENTRY
 }	get_args(int argc, char **argv);
 
 #endif /* !OPTIONS_H_ */
--- src/options.c
+++ src/options.c
@@ -5,7 +5,7 @@
 ** Login SRJanel <n******.******s@epitech.eu>
 ** 
 ** Started on  Sat Oct 14 21:53:05 2017 
-** Last update Fri Nov 24 02:32:12 2017 
+** Last update Mon Dec  4 18:21:51 2017 
 */
 
 #include <unistd.h>
@@ -17,32 +17,33 @@ struct s_options			get_args(int argc, char **argv)
     int					c;
     int					index;
     struct s_options			options = {0};
-    struct option			long_options[] =
+    static struct option			long_options[] =
       {
-	{"username", required_argument, NULL, 'u'},
-	{"password", optional_argument, NULL, 'p'},
-	{"ftp", required_argument, NULL, 'f'},
-	{"target", required_argument, NULL, 't'},
-	{"help", no_argument, &options.help, 1},
+# define ENTRY(FIELD, REQ, SHORTARG) {MAKE_STRING(FIELD), REQ, NULL, SHORTARG},
+	OPTIONS_WITH_ARG_TABLE
+# undef ENTRY
+# define ENTRY(FIELD, SHORTARG) {MAKE_STRING(FIELD), no_argument, NULL, 1},
+	OPTIONS_WITHOUT_ARG_TABLE
+# undef ENTRY
 	{NULL, 0, NULL, 0}
       };
 
     while (1)
       {
 	if ((c = getopt_long(argc, argv,
-			     "t:f:u:p:h",
+			     OPTSTRING,
 			     long_options, &index)) == -1)
 	  break ;
+
 	if (optarg && *optarg == '-') continue ;
-	if (c == 'u')
-	  options.username = optarg;
-	else if (c == 'p')
-	  options.password = optarg;
-	else if (c == 'f')
-	  options.ftp = optarg;
-	else if (c == 't')
-	  options.target = optarg;
-	else if (c == 'h' || c == '?')
+# define ENTRY(FIELD, UNUSED1, SHORTARG) else if (c == SHORTARG && !options.FIELD) \
+	  options.FIELD = optarg;
+	OPTIONS_WITH_ARG_TABLE
+#undef ENTRY
+# define ENTRY(FIELD, SHORTARG) else if (c == SHORTARG) options.FIELD = 1;
+	  OPTIONS_WITHOUT_ARG_TABLE
+#undef ENTRY
+	else if (c == '?')
 	  options.help = 1;
       }
     return (options);
